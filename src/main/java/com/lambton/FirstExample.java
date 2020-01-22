b1f79b5909ef7fc2ef87cfe32ba44ea6b3c33480
MacStudent <macstudent@128-iMAC12.local>
2020-01-21 20:49:14 -0500

    [shree] Initial commit

---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,17 @@
+package com.lambton;
+
+import java.util.Arrays;
+
+public class FirstExample
+{
+    public static void main(String[] args)
+    {
+        System.out.println("welcome to Java");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            System.out.println("shree Marella");
+
+        }
+    }
+}
